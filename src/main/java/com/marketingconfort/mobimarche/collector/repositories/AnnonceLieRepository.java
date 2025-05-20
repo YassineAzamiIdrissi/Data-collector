06c527f19b62b8f276b9e8f1a39ccdee532246d2
ilyasMakhloul <ilyas.makhloul@marketingconfort.com>
2025-05-20 20:54:44 +0100

    correction annonce repo

---
+++ src/main/java/com/marketingconfort/mobimarche/collector/repositories/AnnonceLieRepository.java
@@ -0,0 +1,8 @@
+package com.marketingconfort.mobimarche.collector.repositories;
+
+import com.marketingconfort.mobimarche.collector.models.AnnonceLie;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AnnonceLieRepository extends JpaRepository<AnnonceLie, Integer> {
+
+}
