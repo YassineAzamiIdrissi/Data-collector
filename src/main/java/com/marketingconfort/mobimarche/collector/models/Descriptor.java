43ed99c2c4979ac28a303577e17d30340dca4a26
yassineElazami <yassine.elazami@marketingconfort.com>
2025-05-20 15:03:02 +0100

    adding descc

--- src/main/java/com/marketingconfort/mobimarche/collector/models/Descriptor.java
+++ src/main/java/com/marketingconfort/mobimarche/collector/models/Descriptor.java
@@ -1,4 +1,22 @@
 package com.marketingconfort.mobimarche.collector.models;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Data
+@Entity
 public class Descriptor {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    private String code;
+    @Column(name = "label")
+    private String lebelle;
 }
--- src/main/java/com/marketingconfort/mobimarche/collector/models/MarketType.java
+++ src/main/java/com/marketingconfort/mobimarche/collector/models/MarketType.java
@@ -1,9 +1,6 @@
 package com.marketingconfort.mobimarche.collector.models;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.Id;
-import jakarta.persistence.ManyToOne;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -18,6 +15,7 @@ public class MarketType {
     @Id
     @GeneratedValue
     private Integer id;
+    @Column(name = "marketType")
     private String typeMarche;
 
     @ManyToOne
