86fa57b62b6ca675db02bbab9f0eb68d61350050
yassineElazami <yassine.elazami@marketingconfort.com>
2025-05-20 12:26:34 +0100

    first push

---
+++ src/main/java/com/marketingconfort/mobimarche/collector/CollectorApplication.java
@@ -0,0 +1,13 @@
+package com.marketingconfort.mobimarche.collector;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CollectorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CollectorApplication.class, args);
+	}
+
+}
