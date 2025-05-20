ceae4cfd6c5d76e21f340c3354f91c1867153385
yassineElazami <yassine.elazami@marketingconfort.com>
2025-05-20 20:29:16 +0100

    ..

--- src/main/java/com/marketingconfort/mobimarche/collector/repositories/ValeurRepository.java
+++ src/main/java/com/marketingconfort/mobimarche/collector/repositories/ValeurRepository.java
@@ -3,6 +3,7 @@ package com.marketingconfort.mobimarche.collector.repositories;
 import com.marketingconfort.mobimarche.collector.models.Valeur;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface ValeurRepository extends JpaRepository<Valeur, Integer> {
+public interface ValeurRepository extends JpaRepository<Valeur, Integer>
+{
 
 }
