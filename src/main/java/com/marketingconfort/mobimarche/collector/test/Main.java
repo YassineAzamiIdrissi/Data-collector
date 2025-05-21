1890adb58c91a363da18c7196e2c1ea3e4fb71f6
ilyasMakhloul <ilyas.makhloul@marketingconfort.com>
2025-05-21 18:04:56 +0100

    adding test

---
+++ src/main/java/com/marketingconfort/mobimarche/collector/test/Main.java
@@ -0,0 +1,33 @@
+package com.marketingconfort.mobimarche.collector.test;
+
+import com.marketingconfort.mobimarche.collector.dto.TenderDTO;
+import com.marketingconfort.mobimarche.collector.services.TenderDtoGetterService;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.List;
+
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+
+
+    public static void main(String[] args) {
+
+        RestTemplate restTemplate = new RestTemplate();
+        ////
+
+        // Créez le service
+        TenderDtoGetterService tenderDtoGetterService = new TenderDtoGetterService(restTemplate);
+
+        // Récupérer les offres pour un idweb spécifique
+        List<TenderDTO> offres = tenderDtoGetterService.getTenderDTOById("22_170271");
+        System.out.println("----------------L'offre -------------------------");
+        System.out.println(offres.get(0));
+
+
+    }
+
+}
+
+
