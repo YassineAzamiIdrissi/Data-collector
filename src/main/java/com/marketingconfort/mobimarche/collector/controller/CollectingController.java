24aa468537c4c11f28bf21b7fbc10c6bf026fe0b
yassineElazami <yassine.elazami@marketingconfort.com>
2025-05-21 21:38:03 +0100

    Defining controller

---
+++ src/main/java/com/marketingconfort/mobimarche/collector/controller/CollectingController.java
@@ -0,0 +1,26 @@
+package com.marketingconfort.mobimarche.collector.controller;
+
+import com.marketingconfort.mobimarche.collector.services.TenderDtoGetterService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("collection")
+@RequiredArgsConstructor
+public class CollectingController {
+    private final TenderDtoGetterService service;
+
+    @PostMapping("")
+    ResponseEntity<?> saveTenders(
+            @RequestParam(name = "year") String year,
+            @RequestParam(name = "limit") String limit
+    ) {
+        service.getTendersByYear(year, limit);
+        return ResponseEntity.ok().
+                build();
+    }
+}
