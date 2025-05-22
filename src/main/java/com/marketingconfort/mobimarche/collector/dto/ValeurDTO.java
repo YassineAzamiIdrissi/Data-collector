362abc1d9ff15be39a29c6bb46435b85aefcca2d
yassineElazami <yassine.elazami@marketingconfort.com>
2025-05-22 17:49:47 +0100

    clearences

--- src/main/java/com/marketingconfort/mobimarche/collector/dto/ValeurDTO.java
+++ src/main/java/com/marketingconfort/mobimarche/collector/dto/ValeurDTO.java
@@ -1,11 +1,13 @@
 package com.marketingconfort.mobimarche.collector.dto;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Data;
 
 
 @Data
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class ValeurDTO {
     @JsonIgnore
     private Integer id;
